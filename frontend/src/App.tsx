c9c3692c85cafbab2afc42b956fda7e0d271bf7f
Gkalina-Borisevits <gkalinaborisevits@gmail.com>
2024-05-14 07:07:02 +0200

    feature: Login page with styles and routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout"
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import About from "./pages/about/About";
+import Careers from "./pages/careers/Careers";
 
 const App = () => {
   return (
@@ -9,6 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
         </Route>
       </Routes>
     </div>
